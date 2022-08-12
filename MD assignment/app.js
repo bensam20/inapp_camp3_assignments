b54abbc03b02089e3d842de564740cc165d35585
bensam <ben.ss@inapp.com>
2022-08-12 09:45:40 +0530

    MD Assignment

--- MD assignment/app.js
+++ MD assignment/app.js
@@ -29,3 +29,11 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
 import {MDCSlider} from '@material/slider';
 
 const slider = new MDCSlider(document.querySelector('.mdc-slider'));
+
+
+import {MDCFormField} from '@material/form-field';
+import {MDCRadio} from '@material/radio';
+
+const radio = new MDCRadio(document.querySelector('.mdc-radio'));
+const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+formField.input = radio;
