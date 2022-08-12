469bd05e29e27db1de2c7fc3dc5b4725c804e94e
bensam <ben.ss@inapp.com>
2022-08-12 09:15:33 +0530

    MD Assignment

---
+++ MD assignment/app.js
@@ -0,0 +1,31 @@
+import {MDCRipple} from '@material/ripple/index';
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+import {MDCDialog} from '@material/dialog';
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+
+// import {MDCList} from '@material/list';
+// const list = new MDCList(document.querySelector('.mdc-list'));
+
+import {MDCMenu} from '@material/menu';
+
+const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+menu.open = true;
+
+import {MDCTopAppBar} from '@material/top-app-bar';
+
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+
+
+// const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+// const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+//   return new MDCRipple(el);
+// });
+
+
+import {MDCSlider} from '@material/slider';
+
+const slider = new MDCSlider(document.querySelector('.mdc-slider'));
